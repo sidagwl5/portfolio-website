b5e10c968d116987ca779f053162ed3b7b84dfc5
Siddharth Agrawal <siddharthagrawal@Siddharths-MacBook-Pro.local>
2026-03-02 23:38:33 +0530

    refactor: use SectionDivider from ui components

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,8 @@ import Testimonials from "./components/Testimonials";
 import Projects from "./components/Projects";
 import ContactFooter from "./components/ContactFooter";
 import SmoothScroll from "./components/SmoothScroll";
+import SectionDivider from "./components/ui/SectionDivider";
 import React from "react";
-
-const SectionDivider: React.FC = () => (
-  <div className="w-full max-w-[1080px] mx-auto px-4 md:px-6">
-    <div className="h-px w-full bg-gradient-to-r from-transparent via-slate-300 dark:via-slate-700 to-transparent opacity-80"></div>
-  </div>
-);
 
 export default function Home() {
   return (
